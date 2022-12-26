238ff098a1532d777558e8b783dcffa87c27cdb6
Fernando Rafael Carretto Sosa <fernandocarretto@locus.software>
2022-12-26 13:41:42 -0300

    Several changes on components

---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,49 @@
+<template>
+  <header :style="{ 'background-image': 'url(' + headerImage + ')' }">
+    <div class="logo">
+      <router-link to="/">My Business</router-link>
+    </div>
+    <nav>
+      <ul>
+        <li><router-link to="/products">Products</router-link></li>
+        <li><router-link to="/about">About</router-link></li>
+        <li><router-link to="/contact">Contact</router-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+import headerImage from "@/assets/metallurgy-2932946_960_720.jpg";
+
+export default {
+  name: "HeaderPage",
+  data() {
+    return {
+      headerImage,
+    };
+  },
+};
+</script>
+
+<style>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  height: 100%;
+  background-position: center;
+  background-size: cover;
+}
+.logo {
+  font-size: 1.5em;
+}
+nav ul {
+  display: flex;
+}
+nav li {
+  list-style: none;
+  margin-left: 20px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!-- <div><p>teste</p></div> -->
+    <HeaderPage />
+    <div><p>teste</p></div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderPage from "@/components/HeaderPage.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    HeaderPage,
+  },
+};
 </script>
